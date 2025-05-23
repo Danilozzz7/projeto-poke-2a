b121cfbd57124d4cae7b918c6b95032e912c1159
Danilo Sousa de Freitas Silva <rm93961@estudante.fieb.edu.br>
2025-05-23 11:30:23 +0000

    Grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,19 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
-const Home = () =>{
-
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
+ 
+const Home = () => {
+ 
     return (
         <div>
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
+            <PokeFlex />
+            <PokeGrid />
+           
         </div>
     )
 }
-
+ 
 export default Home
+ 
