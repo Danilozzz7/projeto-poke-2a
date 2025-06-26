a6d586c830c74c9072810b273788ba0eecd64215
Danilo Sousa de Freitas Silva <rm93961@estudante.fieb.edu.br>
2025-06-26 12:56:33 +0000

    ViaCep

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Home from './Home'
 import PokeFlex from './PokeFlex'
 import PokeGrid from './PokeGrid'
 import Menu from './Menu'
+import ViaCep from './ViaCep'
 
 const App = () => {
 
   return (
     <Routes>
       <Route index="/" element={<Menu/>} />
-      <Route path="home/" element={<Home/>} />
+      <Route path="Home/" element={<Home/>} />
       <Route path="PokeFlex/" element={<PokeFlex/>} />
       <Route path="Pokegrid/" element={<PokeGrid/>} />
+      <Route path="ViaCep" element={<ViaCep/>} />
       </Routes>
   )
 }
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,30 +1,26 @@
-
-
-const Menu =()=>{
-
-    return (
-        <div>
-            <ul className="nav">
-            <li class="nav-item">
-            <a class="nav-link active" href="/home">
-                Home
-                   </a>
-                </li>
-
-                <li class="nav-item">
-            <a class="nav-link active" href="/pokeflex">
-                PokeFlex
-                   </a>
-                </li>
-
-                <li class="nav-item">
-            <a class="nav-link active" href="/pokegrid">
-                PokeGrid
-                   </a>
-                </li>
-             </ul>
-        </div>
-
-    )
-}
-export default Menu
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+ 
+const Menu = () => {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="/home">
+          PokeMenu
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="poke-navbar-nav" />
+        <Navbar.Collapse id="poke-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/pokeFlex" >PokeFlex</Nav.Link>
+            <Nav.Link href="/pokegrid">PokeGrid</Nav.Link>
+            <Nav.Link href="/viacep">ViaCep</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+ 
+export default Menu;
+ 
